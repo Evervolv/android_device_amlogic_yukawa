cefdf99b10dfc872fff800d2b98123ff7b167efc
Nick Reuter <nreuter85@gmail.com>
2023-12-08 15:11:20 -0600

    yukawa: add permissions for TV apps
    Change-Id: Icd830b22a192e15a0900a067e46db3d25f395932

--- tvapps/Android.mk
+++ tvapps/Android.mk
@@ -12,6 +12,7 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_BUILT_MODULE_STEM := package.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.google.android.tvlauncher
 
 include $(BUILD_PREBUILT)
 ##############################
@@ -26,5 +27,26 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_BUILT_MODULE_STEM := package.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.google.android.tvrecommendations
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := privapp_whitelist_com.google.android.tvlauncher
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_STEM := com.google.android.tvlauncher.xml
+LOCAL_SRC_FILES := com.google.android.tvlauncher.xml
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := privapp_whitelist_com.google.android.tvrecommendations
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_STEM := com.google.android.tvrecommendations.xml
+LOCAL_SRC_FILES := com.google.android.tvrecommendations.xml
 
 include $(BUILD_PREBUILT)
